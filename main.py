e364aad31f0ff8904ec1e531aeb538bedff89303
MaxTheMooshroom <thepinkmooshroom@gmail.com>
2020-01-08 23:25:29 -0500

    updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Local imports
 from engine import app
-from config import debug
+from .web_app.config import debug
 
 if __name__ == '__main__':
     app.run(debug=debug)
--- web-app/config.py
+++
@@ -1,5 +0,0 @@
-""" Config options for a flask app
-"""
-
-title = "AirBNB Optimal Price Finder"
-secret_key = 'our-secret-key'
